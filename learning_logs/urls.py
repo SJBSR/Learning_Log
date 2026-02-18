511d2b35ef999167a984f9a8066766885650269e
SJBSR <sjb.sr@outlook.com>
2026-02-18 09:09:22 -0500

    P2

---
+++ learning_logs/urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+]
